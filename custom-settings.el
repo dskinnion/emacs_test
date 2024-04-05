30d4135148b70c0f4400b463d6bf63ee02719279
Dominic Skinnion <dominic_skinnion@iq.harvard.edu>
2024-04-05 12:34:20 -0400

    Initial files (excluding elpa packages)

---
+++ custom-settings.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ox-pandoc pandoc-mode ein magit git-commit ivy-bibtex auctex conda poetry poly-R ess windresize diff-hl adaptive-wrap pdf-tools yasnippet yasnippet-snippets multiple-cursors visual-regexp command-log-mode better-defaults minions ace-window ctable yaml-mode deferred epc s rotate howdoi multi-term with-editor anzu counsel flx-ido smex hydra ivy-hydra wgrep which-key outline-magic smooth-scroll unfill company undo-fu company-math company-auctex web-mode markdown-mode polymode poly-markdown poly-org poly-noweb eval-in-repl exec-path-from-shell dumb-jump flycheck htmlize dictionary untitled-new-buffer)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
